c08236368331ffb78216637f1a4967c643c36217
TristanSheedy <tsheedy@integral.com.au>
2026-05-15 15:28:07 +1000

    Adding missed files

---
+++ amq_producer/src/main/java/com/example/amq_producer/AmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.amq_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqProducerApplication.class, args);
+	}
+
+}
---
+++ amq_producer/src/test/java/com/example/amq_producer/AmqProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.amq_producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
